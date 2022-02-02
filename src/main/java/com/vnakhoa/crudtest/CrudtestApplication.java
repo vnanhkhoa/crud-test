1a4529ee1478d93d636ecf7d225862591391d091
VNANHKHOA <anhkhoadn20@gmail.com>
2022-02-02 23:17:37 +0700

    fix upload1

--- src/main/java/com/vnakhoa/crudtest/CrudtestApplication.java
+++ src/main/java/com/vnakhoa/crudtest/CrudtestApplication.java
@@ -7,11 +7,7 @@ import java.net.URL;
 
 @SpringBootApplication
 public class CrudtestApplication {
-    public static String URL_IMAGE = "";
     public static void main(String[] args) {
-        ClassLoader classLoader = CrudtestApplication.class.getClassLoader();
-        URL url = classLoader.getResource("static");
-        URL_IMAGE = url.getPath();
         SpringApplication.run(CrudtestApplication.class, args);
     }
 
